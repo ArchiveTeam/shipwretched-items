37f63d5e626f6a16c2b4e5c6923a27cb9542d93c
Christopher Foo <chris.foo@gmail.com>
2013-12-31 15:32:53 +0800

    make_patch.py: support --lowercase option.

--- make_batch.py
+++ make_batch.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('all')
     arg_parser.add_argument('done', nargs='+')
+    arg_parser.add_argument('--lowercase', action='store_true')
 
     args = arg_parser.parse_args()
 
@@ -14,11 +15,15 @@ if __name__ == '__main__':
 
     with open(args.all) as in_file:
         for line in in_file:
+            if args.lowercase:
+                line = line.lower()
             all_items.add(line.strip())
 
     for path in args.done:
         with open(path) as in_file:
             for line in in_file:
+                if args.lowercase:
+                    line = line.lower()
                 done_items.add(line.strip())
 
     for item in sorted(all_items - done_items):
